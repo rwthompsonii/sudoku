a705e9980ae6156b166a16f0a70158ca385ca261
Ron T <rwthompsonii@gmail.com>
2017-08-13 21:06:20 -0700

    and it worksgit add sudokuSolver.py

--- src/sudokuSolver.py
+++ src/sudokuSolver.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
-import sys, logging, argparse, pprint, random
+import sys, logging, argparse, pprint, random, math
 from  tkinter import *
 from functools import partial
+from operator import attrgetter
+
+#constants
 DEBUG = False
 MAX_ROWS_COLUMNS=15
+
+class PotentialValue():
+    def __init__(self, row, column, value):
+        self.row = row
+        self.column = column
+        self.value = value
+
+    def __repr__():
+        return 'PotentialValue(row = %s, column = %s, value = %s) % self.row, self.column, self.value'
 
 class SudokuButton(Button):
     def __init__(self, row, column, master=None, **options):
@@ -13,8 +25,18 @@ class SudokuButton(Button):
         self.column = column
         self.value = 0 #the zero value is used to indicate that this is an invalid square to the backtracker.
         self.textvariable = StringVar()
-        self.configure(textvariable = self.textvariable, width=2, height=2)
         self.hard_set = False #used to indicate that this is part of the initial condition and should not be reset when backtracking/solving.
+        self.configure(textvariable = self.textvariable, width=2, height=2, command=self.on_button_clicked)
+
+    def set_init(self, value):
+        self.set_value(value)
+        self.hard_set = True
+        self.configure(bg = "gray")
+
+    def unset_init(self):
+        self.hard_set = False
+        self.set_value(0)
+        self.configure(bg = "white")
 
     def set_value(self, value):
         self.value = value
@@ -22,9 +44,16 @@ class SudokuButton(Button):
             self.textvariable.set(str(value)) 
         else:
             self.textvariable.set("")
+    
+    def on_button_clicked(self):
+        debug_print("button clicked, location: [{0.row},{0.column}]".format(self))
+        debug_print(self.__repr__())
 
     def __repr__(self):
         return 'SudokuButton(row=%s, column=%s, value=%s, textvariable=%s, hard_set=%s)' % (self.row, self.column, self.value, self.textvariable.get(), self.hard_set)
+
+    def __str__(self):
+        return self.__repr__()
 
 #TODO: these need to come from user input or at least a .conf file
 def get_initial_values():
@@ -41,9 +70,85 @@ def get_initial_values():
 def debug_print_button_array(button_array):
     debug_print(list(button_array), True)
 
+def isqrt(n):
+    #from https://stackoverflow.com/questions/15390807/integer-square-root-in-python/17495624
+    #i don't really care that it overflows on large values because the input size is sanitized
+    #much earlier.
+    i = int(math.sqrt(n) + 0.5)
+    if i**2 == n:
+        return i
+    raise ValueError('input rows/columns were not a perfect square')
+
+#this function is the only one that actually requires a perfect square input.
+def isValueInSameBlock(button_array, pot_value):
+    max_row = max(button_array, key=attrgetter('row')).row
+    max_column = max(button_array, key=attrgetter('column')).column
+    
+    #these two statements ensure that the given inputs make nice blocks.
+    assert max_row == max_column
+    block_size = isqrt(max_row + 1)
+
+    #the modulus will be zero if i'm already on the starting row/column.
+    start_row = pot_value.row - (pot_value.row % block_size)
+    start_column = pot_value.column - (pot_value.column % block_size)
+
+    #end row is not inclusive, it's actually the start of the next block
+    end_row = start_row + block_size
+    end_column = start_column + block_size
+
+    for button in button_array:
+        if button.row >= start_row and button.row < end_row and button.column >= start_column and button.column < end_column and button.value == pot_value.value:
+            return True
+
+    return False
+
+def isValueInRowOrColumn(button_array, pot_value):
+    for button in button_array:
+        if button.column != pot_value.column and button.row == pot_value.row and button.value == pot_value.value: #don't check the same button...
+            return True
+        if button.row != pot_value.row and button.column == pot_value.column and button.value == pot_value.value: #don't check the same button... 
+            return True
+
+    return False
+
+def isNewValueValid(button_array, pot_value):
+    return not isValueInSameBlock(button_array, pot_value) and not isValueInRowOrColumn(button_array, pot_value) 
+
 def solve_action(button_array):
     debug_print("Solve button clicked.")
-    debug_print_button_array(button_array)
+    #debug_print_button_array(button_array)
+    solved = guess(button_array)
+    if solved == True:
+        debug_print("All done.")
+
+def guess(button_array):
+    first_empty = next(filter(lambda i: i.value == 0 and i.hard_set == False, button_array), None)
+    debug_print("working on first_empty: %s" % (first_empty))
+    if first_empty is not None:
+        #debug_print(first_empty.__repr__())
+        randoms = list(range(1,10))
+        random.shuffle(randoms)
+        #debug_print(randoms)
+        
+        for r in randoms:
+            pot_value = PotentialValue(first_empty.row, first_empty.column, r)
+            if isNewValueValid(button_array, pot_value):
+                for button in button_array:
+                    if button.row == first_empty.row and button.column == first_empty.column:
+                        button.set_value(pot_value.value)
+                if guess(button_array):
+                   return True #start unwinding
+                else:
+                    for button in button_array:
+                        if button.row == first_empty.row and button.column == first_empty.column:
+                            button.set_value(0)
+        
+        #if we make it here then none of the random values were valid.  start unwinding.
+        return False
+    else:
+        return True #all done, the remaining values are filled.
+
+    
 
 def startup_ui(rows, columns):
     root = Tk()
@@ -57,7 +162,13 @@ def startup_ui(rows, columns):
     for r in range(rows):
         for c in range(columns):
             button = SudokuButton(r, c, root, borderwidth = 1)
-            button.set_value(init_values[r][c])
+            
+            #TODO: all this init code is just to debug the solver.  Need to init smarter.
+            init_value = init_values[r][c]
+            if not init_value == 0:
+                button.set_init(init_values[r][c])
+                button.hard_set = True
+            
             button.grid(row = r,column = c)
             button_array.append(button)
 
